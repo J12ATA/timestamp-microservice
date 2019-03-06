786f3d3608374b7456e04495559c427c6ce89c6d
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-03-06 01:24:05 -0800

    edits to server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-// server.js where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing) so that your API is remotely testable by FCC 
 const cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public')); // http://expressjs.com/en/starter/static-files.html
 
 app.set('view engine', 'pug');
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
-  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-  res.render('index', { fullUrl });
-});
+app.get('/', (req, res) => res.render('index')); // http://expressjs.com/en/starter/basic-routing.html
 
 // your first API endpoint... 
-app.get('/api/hello', (req, res) => {
-  res.json({greeting: 'hello API'});``
-});
+app.get('/api/hello', (req, res) => res.json({ greeting: 'hello API' }));
 
 app.get('/api/timestamp/:date_string?', (req, res) => {
   const date_string = req.params.date_string;
